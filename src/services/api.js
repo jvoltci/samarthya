4105bf1557ad1c3bf5554ac5aeecb440e2ac653d
JAI <jai@fiveoak.com>
2025-02-26 01:23:58 +0530

    Update: API endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create a new axios instance
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080/api/v1',
+  baseURL: 'https://samarthyax.onrender.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
